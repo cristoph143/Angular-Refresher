fde97b785fd7e5964bfa064bec9dc70f96a11336
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:28:14 +0000

    badge components

--- src/app/components/ui/data-display/badge/badge.component.ts
+++ src/app/components/ui/data-display/badge/badge.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-badge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './badge.component.html',
-  styleUrl: './badge.component.css'
+  styleUrls: ['./badge.component.css']
 })
 export class BadgeComponent {
-
+  @Input() label: string = '';
+  @Input() type: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' = 'primary';
 }
