52c3e4b7e8bad9b658374418ffdcaf33075a6bf4
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:34:31 +0000

    toast notif components

--- src/app/components/ui/feedback/toast-notification/toast-notification.component.ts
+++ src/app/components/ui/feedback/toast-notification/toast-notification.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toast-notification',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toast-notification.component.html',
-  styleUrl: './toast-notification.component.css'
+  styleUrls: ['./toast-notification.component.css']
 })
 export class ToastNotificationComponent {
+  @Input() message: string = '';
+  @Input() type: 'success' | 'error' | 'warning' | 'info' = 'info';
+  @Input() duration: number = 3000;
 
+  visible: boolean = true;
+
+  ngOnInit() {
+    setTimeout(() => this.visible = false, this.duration);
+  }
 }
