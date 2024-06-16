1d063191d1abb3498625822bece2555699ef941e
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:33:00 +0000

    progress bar components

--- src/app/components/ui/misc/progress-bar/progress-bar.component.ts
+++ src/app/components/ui/misc/progress-bar/progress-bar.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './progress-bar.component.html',
-  styleUrl: './progress-bar.component.css'
+  styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent {
-
+  @Input() value: number = 0;
 }
