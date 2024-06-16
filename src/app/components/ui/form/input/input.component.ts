bb72d93099021b91c31f45b49e7847ea62b6de29
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:24:42 +0000

    input components

--- src/app/components/ui/form/input/input.component.ts
+++ src/app/components/ui/form/input/input.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() value: string = '';
 }
