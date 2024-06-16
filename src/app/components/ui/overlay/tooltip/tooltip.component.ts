f73fc92770955c89ed1e7cf583551471455672df
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:26:41 +0000

    tooltip components

--- src/app/components/ui/overlay/tooltip/tooltip.component.ts
+++ src/app/components/ui/overlay/tooltip/tooltip.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tooltip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tooltip.component.html',
-  styleUrl: './tooltip.component.css'
+  styleUrls: ['./tooltip.component.css']
 })
 export class TooltipComponent {
-
+  @Input() message: string = '';
 }
