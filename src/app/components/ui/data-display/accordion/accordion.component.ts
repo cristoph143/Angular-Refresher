c259411768eba12c10c9b956cb3a16aae1642cc7
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:32:11 +0000

    accordion components

--- src/app/components/ui/data-display/accordion/accordion.component.ts
+++ src/app/components/ui/data-display/accordion/accordion.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accordion.component.html',
-  styleUrl: './accordion.component.css'
+  styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent {
+  @Input() items: { title: string, content: string }[] = [];
+  selectedItem: number = -1;
 
+  toggleItem(index: number) {
+    this.selectedItem = this.selectedItem === index ? -1 : index;
+  }
 }
