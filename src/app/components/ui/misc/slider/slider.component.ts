b2febdc57cee3cde28fbb78e49333e97cefb6822
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:33:41 +0000

    slider components

--- src/app/components/ui/misc/slider/slider.component.ts
+++ src/app/components/ui/misc/slider/slider.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
-
+  @Input() min: number = 0;
+  @Input() max: number = 100;
+  @Input() value: number = 50;
 }
