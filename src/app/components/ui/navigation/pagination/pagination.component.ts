14ba47fce4439b22eb4f7f1f0df7ab1fa4bd1b4e
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:37:26 +0000

    pagination components

--- src/app/components/ui/navigation/pagination/pagination.component.ts
+++ src/app/components/ui/navigation/pagination/pagination.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.css'
+  styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
+  @Input() totalPages: number = 1;
+  @Input() currentPage: number = 1;
 
+  get pages(): number[] {
+    return Array(this.totalPages).fill(0).map((_, i) => i + 1);
+  }
 }
