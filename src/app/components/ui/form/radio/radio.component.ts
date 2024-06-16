572ae7425f301be71bb2d3167c2dc3af94123fcf
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:24:49 +0000

    radio components

--- src/app/components/ui/form/radio/radio.component.ts
+++ src/app/components/ui/form/radio/radio.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-radio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './radio.component.html',
-  styleUrl: './radio.component.css'
+  styleUrls: ['./radio.component.css']
 })
 export class RadioComponent {
-
+  @Input() name: string = '';
+  @Input() label: string = '';
+  @Input() value: string = '';
+  @Input() checked: boolean = false;
 }
