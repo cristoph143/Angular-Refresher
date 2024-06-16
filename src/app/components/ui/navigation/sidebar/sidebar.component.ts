058bc7cadd4273e387786b67764c7c71af9b2623
cris.bacus6 <cris.bacus6@gmail.com>
2024-06-16 16:36:00 +0000

    sidebar components

--- src/app/components/ui/navigation/sidebar/sidebar.component.ts
+++ src/app/components/ui/navigation/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent { }
